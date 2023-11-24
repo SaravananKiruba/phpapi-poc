b4ee336df1784ef0126ebd7ef98d55fe8799940e
Saravanan Kiruba S <saravanan.s@altrockstech.com>
2023-11-24 15:34:48 +0530

    pdf download -- intiate - done -- kiruba

--- baleen-api/resources/views/pdf/view.blade.php
+++ baleen-api/resources/views/pdf/view.blade.php
@@ -1,53 +1,97 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Work Order with Terms and Conditions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Details - Invoice</title>
     <style>
-        /* Add your custom CSS styles here */
         body {
             font-family: Arial, sans-serif;
+            margin: 20px;
         }
-        
-        .header {
+
+        header {
             text-align: center;
-            margin-bottom: 20px;
+            padding: 10px;
+            background-color: #f2f2f2;
         }
-        
-        .logo {
-            width: 200px; /* Adjust the size of your logo */
-            height: auto;
+
+        section {
+            margin: 20px 0;
         }
-        
-        .terms {
-            margin-bottom: 20px;
-        }
-        
-        .instructions {
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
             margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .total {
+            font-weight: bold;
+        }
+
+        footer {
+            margin-top: 20px;
+            text-align: center;
         }
     </style>
 </head>
-
 <body>
-    <div class="header">
-        <img src="path/to/your/logo.png" alt="Your Logo" class="logo">
-    </div>
 
-    <div class="terms">
-        <!-- Replace this section with your actual terms and conditions content -->
+    <header>
+        <h1>Order Details - Invoice</h1>
+    </header>
+
+    <section>
+        <h2>Order Information</h2>
+        <table>
+            <tr>
+                <th>Order Number</th>
+                <td>{{ $data->OrderNumber }}</td>
+            </tr>
+            <!-- Add more rows for other order information using $data->fieldName -->
+        </table>
+    </section>
+
+    <section>
+        <h2>Client Details</h2>
+        <table>
+            <tr>
+                <th>Client Name</th>
+                <td>{{ $data->ClientName }}</td>
+            </tr>
+            <!-- Add more rows for other client details using $data->fieldName -->
+        </table>
+    </section>
+
+    <section>
+        <h2>Payment Details</h2>
+        <p><strong>Gross Amount:</strong> Rs.{{ $data->Receivable }}</p>
+        <p><strong>GST ({{ $data->GST }}%):</strong> Rs.{{ $data->Receivable * ($data->GST / 100) }}</p>
+        <p class="total"><strong>Total Amount:</strong> Rs.{{ $data->Receivable + ($data->Receivable * ($data->GST / 100)) }}</p>
+    </section>
+
+    <section>
         <h2>Terms and Conditions</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor ante vitae nunc lacinia tristique. Sed ut dui nec justo aliquet ultrices.</p>
-    </div>
+        <p>{{ $data->{'Special T & C'} }}</p>
+    </section>
 
-    <div class="instructions">
-       <!-- Replace this section with your ultimate instructions for the topic -->
-       <h2>Ultimate Instructions for [TOPIC]</h2>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor ante vitae nunc lacinia tristique. Sed ut dui nec justo aliquet ultrices.</p>
-   </div>
+    <footer>
+        <p>Thank you for choosing our services!</p>
+    </footer>
 
 </body>
-
 </html>
-
-
